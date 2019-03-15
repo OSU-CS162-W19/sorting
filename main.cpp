5df9445a93c72af4dbc2de58a83dbdefeee521fc
Rob Hess <robwhess@gmail.com>
2019-03-15 13:11:38 -0700

    Add code from 3/15 lecture.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include <vector>
 #include <cstdlib>
 
+int factorial(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n-1);
+}
+
+int fibonacci(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
 template <class T>
 void swap(T& a, T& b) {
   T tmp = a;
@@ -40,6 +54,39 @@ void insertion_sort(std::vector<T>& array) {
   }
 }
 
+template <class T>
+int partition(std::vector<T>& array, int lo, int hi) {
+  T pivot = array[lo];
+  while (true) {
+    while (array[lo] < pivot) {
+      lo++;
+    }
+    while (array[hi] > pivot) {
+      hi--;
+    }
+    if (lo >= hi) {
+      return hi;
+    }
+    swap(array[lo], array[hi]);
+  }
+}
+
+
+template <class T>
+void quicksort_recursive(std::vector<T>& array, int lo, int hi) {
+  if (lo < hi) {
+    int p = partition(array, lo, hi);
+    quicksort_recursive(array, lo, p-1);
+    quicksort_recursive(array, p+1, hi);
+  }
+}
+
+
+template <class T>
+void quicksort(std::vector<T>& array) {
+  quicksort_recursive(array, 0, array.size() - 1);
+}
+
 
 int main() {
   std::vector<int> vec;
@@ -55,7 +102,8 @@ int main() {
 
   // Sort the data...
   // selection_sort(vec);
-  insertion_sort(vec);
+  // insertion_sort(vec);
+  quicksort(vec);
 
   std::cout << "After sorting: ";
   for (int i = 0; i < vec.size(); i++) {
